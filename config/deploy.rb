37085b932b9b8d5444cd4df8da9931f1e574f758
NateRive <naterive1125@gmail.com>
2018-05-13 15:47:59 +0900

    TEST

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,13 +3,14 @@ lock "~> 3.10.2"
 
 set :application, "quizlet"
 set :repo_url, "git@github.com:NateRive/quizlet.git"
-set :branch, "master"
+# set :linked_files, %w{ config/secrets.yml }
 
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 # リモートのreleasesフォルダに羅列したファイルを作成
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
+
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['/Users/naterive/.ssh/nate_rive.pem']
